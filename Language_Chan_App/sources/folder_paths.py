41a905ebd281cb10da91cd7b3d9deb993d468b84
PlumJE <lje64257@gmail.com>
2024-06-27 22:57:29 +0900

    Recently commited

--- Language_Chan_App/sources/folder_paths.py
+++ Language_Chan_App/sources/folder_paths.py
@@ -2,13 +2,13 @@
 from os import makedirs
 from os.path import dirname, isdir, realpath
 
-this_app_dir = dirname(realpath(__file__)) + '/'
-db_folder = this_app_dir + 'databases/'
-graphics_folder = this_app_dir + 'graphics/'
+app_dir = dirname(dirname(realpath(__file__))) + '/'
+db_folder = app_dir + 'databases/'
+graphics_folder = app_dir + 'graphics/'
 bg_folder = graphics_folder + 'background/'
 charas_folder = graphics_folder + 'charas/'
 enemies_folder = graphics_folder + 'enemies/'
-GUI_folder = this_app_dir + 'GUIfiles/'
+GUI_folder = app_dir + 'GUIfiles/'
 
 if not isdir(db_folder):
     makedirs(db_folder)
--- NoTwitter_App/sources/folder_paths.py
+++ NoTwitter_App/sources/folder_paths.py
@@ -2,10 +2,10 @@
 from os import makedirs
 from os.path import dirname, isdir, realpath
 
-this_app_dir = dirname(realpath(__file__))
-db_folder = this_app_dir + '/databases/'
-graphics_folder = this_app_dir + '/graphics/'
-GUI_folder = this_app_dir + '/GUIfiles/'
+app_dir = dirname(dirname(realpath(__file__)))
+db_folder = app_dir + '/databases/'
+graphics_folder = app_dir + '/graphics/'
+GUI_folder = app_dir + '/GUIfiles/'
 
 if not isdir(db_folder):
     makedirs(db_folder)
