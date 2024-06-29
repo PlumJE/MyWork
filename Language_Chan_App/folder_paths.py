7a2acd812fb0827dd1114cbc04df4e3db3971dfa
PlumJE <lje64257@gmail.com>
2024-06-29 16:13:21 +0900

    Recently commited

--- Language_Chan_App/folder_paths.py
+++ Language_Chan_App/folder_paths.py
@@ -1,10 +1,13 @@
-# 폴더 경로 상수
+#폴더 경로 상수들입니다.
 from os import makedirs
 from os.path import dirname, isdir, realpath
 
-app_dir = dirname(dirname(realpath(__file__)))
+app_dir = dirname(realpath(__file__))
 db_folder = app_dir + '/databases/'
 graphics_folder = app_dir + '/graphics/'
+bg_folder = graphics_folder + '/background/'
+charas_folder = graphics_folder + '/charas/'
+enemies_folder = graphics_folder + '/enemies/'
 GUI_folder = app_dir + '/GUIfiles/'
 
 if not isdir(db_folder):
---
+++ NoTwitter_App/folder_paths.py
@@ -0,0 +1,11 @@
+# 폴더 경로 상수
+from os import makedirs
+from os.path import dirname, isdir, realpath
+
+app_dir = dirname(realpath(__file__))
+db_folder = app_dir + '/databases/'
+graphics_folder = app_dir + '/graphics/'
+GUI_folder = app_dir + '/GUIfiles/'
+
+if not isdir(db_folder):
+    makedirs(db_folder)
